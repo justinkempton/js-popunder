c221fd167c0ba4457c0966e06783d45b1f44f96a
justinkempton <justin@kempton.com>
2013-07-16 10:47:13 -0700

    Update jspopunder.js
    fixed bug with ie8 which was did not support addEventListener. http://stackoverflow.com/questions/6927637/addeventlistener-in-internet-explorer

--- jspopunder.js
+++ jspopunder.js
@@ -54,20 +54,26 @@ function jsPopunder(sUrl, sConfig) {
 
         var sOptions = 'toolbar=no,scrollbars=yes,location=yes,statusbar=yes,menubar=no,resizable=1,width=' + sWidth.toString() + ',height=' + sHeight.toString() + ',screenX=' + sPosX + ',screenY=' + sPosY;
 
-        document.addEventListener("click", function() {
-            if (isCapped()) return;
+    			var listenerEvent = function() {
+					if (isCapped()) return;
 
-            popunder = _parent.window.open(sUrl, sName, sOptions);
-            if (popunder) {
-                // cookie
-                var now  = new Date();
-                var next = new Date(now.setTime(now.getTime() + sWait));
-                document.cookie = cookie + '=1;expires=' + next.toGMTString() + ';path=/';
-                var tomorrow = new Date(); tomorrow.setHours(24,0,0,0);
-                document.cookie = cookie + 'Cap=' + (popsToday + 1) + ';expires=' + tomorrow.toGMTString() + ';path=/';
-                pop2under();
-            }
-        }, false);
+					popunder = _parent.window.open(sUrl, sName, sOptions);
+					if (popunder) {
+							// cookie
+							var now  = new Date();
+							var next = new Date(now.setTime(now.getTime() + sWait));
+							document.cookie = cookie + '=1;expires=' + next.toGMTString() + ';path=/';
+							var tomorrow = new Date(); tomorrow.setHours(24,0,0,0);
+							document.cookie = cookie + 'Cap=' + (popsToday + 1) + ';expires=' + tomorrow.toGMTString() + ';path=/';
+							pop2under();
+					}
+				}
+
+				if (document.addEventListener) {
+					document.addEventListener("click", listenerEvent, false);
+				} else {
+					document.attachEvent("onclick", listenerEvent);
+				}
     }
 
 
